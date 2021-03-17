1a997cc945cb1c1eac7dcc02993213a22aa5dccd
Shiver <Shiver@wangxutech.com>
2021-03-17 16:50:13 +0800

    commit VS2015 build so project,and test with AS app

---
+++ VSBuildSoTest/ASTestApp/app/src/main/java/com/example/helloso/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.helloso;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("SharedObject1");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = findViewById(R.id.sample_text);
+        tv.setText(HelloWorld());
+    }
+
+
+
+    public static native String getPlatformABI();
+
+    public static native String HelloWorld();
+}
